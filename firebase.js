5b55bb4904c80555a3b4083b4719d9ec110e7d4d
Ressent <ressentoff@gmail.com>
2025-05-28 01:01:29 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// Configuración Firebase (rellenar con tus datos reales)
+  const firebaseConfig = {
+    apiKey: "AIzaSyB_yrH-TyDxl1XW2N050i7ztMrB9S58tJI",
+    authDomain: "sapd-page.firebaseapp.com",
+    projectId: "sapd-page",
+    storageBucket: "sapd-page.firebasestorage.app",
+    messagingSenderId: "1043996553575",
+    appId: "1:1043996553575:web:5a4ba5166e0c1d1f0fbfc2",
+    measurementId: "G-NCXKQ05S2Q"
+  };
+  firebase.initializeApp(firebaseConfig);
